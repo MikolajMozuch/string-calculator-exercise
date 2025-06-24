f98c2fa0f03f4d8a824271a94e947c25f5df13ae
Mikołaj Możuch <mozuch.mikolaj@gmail.com>
2025-06-24 14:40:30 +0200

    Refactor of InputValidatorTest

--- src/test/java/InputValidatorTest.java
+++ src/test/java/InputValidatorTest.java
@@ -1,46 +1,50 @@
-import exception.IncorrectInputFormatException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import services.CustomStringDelimiterParser;
 import services.InputValidator;
+import services.StringCalculatorService;
+import services.StringCalculatorServiceImpl;
 
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class InputValidatorTest {
-
     @Test
-    public void addCommaSeparatorAtEndShouldThrowException() {
-        var exception = assertThrows(IncorrectInputFormatException.class, () ->
-                InputValidator.isSeparatorAtTheEnd("1,2,", CustomStringDelimiterParser.DELIMITER_COMMA));
-        assertEquals("Input data cannot end with the separator", exception.getMessage());
+    public void addCommaSeparatorAtEndShouldAddError() {
+        List<String> errors = new ArrayList<>();
+        InputValidator.checkEndsWithDelimiter("1,2,", CustomStringDelimiterParser.DELIMITER_COMMA, errors);
+        assertTrue(errors.contains("Input data cannot end with the separator"));
     }
 
     @Test
-    public void addNewlineSeparatorAtEndShouldThrowException() {
-        var exception = assertThrows(IncorrectInputFormatException.class, ()
-                -> InputValidator.isSeparatorAtTheEnd("1\n2\n", CustomStringDelimiterParser.DELIMITER_NEW_LINE));
-        assertEquals("Input data cannot end with the separator", exception.getMessage());
+    public void addNewlineSeparatorAtEndShouldAddError() {
+        List<String> errors = new ArrayList<>();
+        InputValidator.checkEndsWithDelimiter("1\n2\n", CustomStringDelimiterParser.DELIMITER_NEW_LINE, errors);
+        assertTrue(errors.contains("Input data cannot end with the separator"));
     }
-    // 6)
+
+    //6)
     @Test
-    public void verifyNegativeNumbersInputContainsOneNegativeNumberThrowException() {
-        int[] input = {1, -2, 3};
-        var exception = assertThrows(IncorrectInputFormatException.class, () ->
-                InputValidator.verifyNoNegativeNumbers(input));
-        assertEquals("Negative number(s) not allowed: -2", exception.getMessage());
+    public void verifyNegativeNumbersInputContainsOneNegativeNumberAddsError() {
+        List<String> errors = new ArrayList<>();
+        InputValidator.checkNegativeNumbers("1,-2", CustomStringDelimiterParser.DELIMITER_COMMA, errors);
+        assertTrue(errors.contains("Negative number(s) not allowed: -2"));
     }
 
     @Test
-    public void verifyNegativeNumbersInputContainsMultipleNegativeNumbersThrowException() {
-        int[] input = {1, -2, -3, -4};
-        var exception = assertThrows(IncorrectInputFormatException.class, () ->
-                InputValidator.verifyNoNegativeNumbers(input));
-        assertEquals("Negative number(s) not allowed: -2, -3, -4", exception.getMessage());
+    public void verifyNegativeNumbersInputContainsMultipleNegativeNumbersAddsError() {
+        List<String> errors = new ArrayList<>();
+        InputValidator.checkNegativeNumbers("2,-4,-9", CustomStringDelimiterParser.DELIMITER_COMMA, errors);
+        assertTrue(errors.contains("Negative number(s) not allowed: -4, -9"));
     }
 
     @Test
-    public void verifyNegativeNumbersInputContainsOnlyPositiveNumbersDoesNotThrowException() {
-        int[] input = {1, 2, 3};
-        assertDoesNotThrow(() -> InputValidator.verifyNoNegativeNumbers(input));
+    public void verifyNegativeNumbersInputContainsOnlyPositiveNumbersAddsNoError() {
+        List<String> errors = new ArrayList<>();
+        InputValidator.checkNegativeNumbers("1,2,3", CustomStringDelimiterParser.DELIMITER_COMMA, errors);
+        assertTrue(errors.isEmpty());
     }
 }
